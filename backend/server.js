193015b57bcec23f0c983b2cb194c836657c3b92
DmytroTarasiuk <dmytro.tarasiuk1899@gmail.com>
2021-09-10 16:06:56 +0200

    connecting to a database

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,43 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import products from'./data/products.js'
+
+import connectDB from './config/db.js'
+import colors from 'colors'
+import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleWare.js'
+
 
 dotenv.config()
 const app = express()
 
+connectDB()
+
 app.get('/', (req,res) => {
     res.send('API is running')
 })
+
+app.use('/api/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
+
+/*
+app.use((req,res,next) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`)
+    res.status(404)
+    next(error)
+})
+
+app.use((err, req,res,next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+    res.status(statusCode)
+    res.json({
+        message: err.message,
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+        
+    })
+})
+
 
 app.get('/api/products', (req,res) => {
     res.json(products)
@@ -17,7 +47,8 @@ app.get('/api/products/:id', (req,res) => {
     const product = products.find(p => p._id === req.params.id)
     res.json(product)
 })
+*/
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}..`))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}..`.yellow.bold))
