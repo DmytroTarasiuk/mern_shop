940520759bac54886e3f5c273599b013f88d2917
DmytroTarasiuk <dmytro.tarasiuk1899@gmail.com>
2021-09-26 19:42:09 +0200

    back end user auth

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,12 @@ import connectDB from './config/db.js'
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleWare.js'
-
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 const app = express()
+
+app.use(express.json())
 
 connectDB()
 
@@ -17,6 +19,7 @@ app.get('/', (req,res) => {
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
